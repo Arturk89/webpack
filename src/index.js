5022e9737af496a9ada4a8433fe9ec4f564114d0
Artur Kempny <artur.kempny@gmail.com>
2020-04-02 11:55:43 +0200

    Created tamplate project

--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
-import { message, messageDOM } from "./tools/message";
-import "./components/footer";
-import info from "./data/title.txt";
 import "./sass/index.scss";
-import addImage from "./tools/image";
-import Creator from "./tools/creator";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("app"));
-
-// message(info);
-// messageDOM(info);
-// addImage("h1");
-
-// const e1 = new Creator();
-// e1.addBgc("red");
-// const e2 = new Creator();
-// e2.addBgc("blue");
-// const e3 = new Creator();
-// e3.addBgc();
-// e3.showColor();
